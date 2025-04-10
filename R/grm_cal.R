56c9352c453fc4ca4bbb757750fb8a1df82706f0
dnpthanh <82787669+dnpthanh@users.noreply.github.com>
2025-04-10 14:03:10 +0900

    Update grm_cal.R

--- R/grm_cal.R
+++ R/grm_cal.R
@@ -13,22 +13,39 @@
 #' @export
 grm_cal <- function(freq_table, geno_table, weight_vec = NULL) {
     # Frequency file
-    freq <- freq_table[!is.na(freq_table$MAF), ]
+    if (any(is.na(freq_table$MAF))) {
+        stop("There is NA(s) in frequency file.")
+    }
+    
+    freq <- freq_table
     
     # Genotype file
-    geno <- geno_table
-    n <- nrow(geno)
-    m <- ncol(geno)
-    p <- m - 6  # number of SNP
+    if (any(is.na(geno_table))) {
+        stop("There is NA(s) in genotype file. Imputation may be needed.")
+    }
+
+    geno_snp_ids <- colnames(geno_table)[7:ncol(geno_table)]
+    common_snp <- intersect(geno_snp_ids, freq$SNP)
+
+    if (length(common_snp) == 0) {
+        stop("No common SNPs between freq and geno tables. Please check SNP IDs.")
+    }
+
+    freq_matched <- freq[match(common_snp, freq$SNP), ]
+    geno_matched <- geno_table[, c(1:6, match(common_snp, colnames(geno_table)))]
+
+    n <- nrow(geno_matched) #number of animals
+    p <- length(common_snp) #number of SNPs
 
     # Prepare M matrix (adjusted -2p)
-    geno_adj <- geno[, 7:m] - 2 * matrix(freq$MAF, nrow = n, ncol = p, byrow = TRUE)
+    geno_adj <- as.matrix(geno_matched[, 7:ncol(geno_matched)]) - 
+                2 * matrix(freq_matched$MAF, nrow = n, ncol = p, byrow = TRUE)
 
     # Weights (default = 1)
     wg <- if (!is.null(weight_vec)) weight_vec else rep(1, p)
 
     # Denominator
-    bot <- 2 * sum(freq$MAF * (1 - freq$MAF))
+    bot <- 2 * sum(freq_matched$MAF * (1 - freq_matched$MAF))
 
     # MDM'
     grm_matrix <- (as.matrix(geno_adj) %*% diag(wg) %*% t(as.matrix(geno_adj))) / bot
@@ -42,6 +59,5 @@ grm_cal <- function(freq_table, geno_table, weight_vec = NULL) {
         ID_2 = geno[idx[, 2], 2],
         Rela = grm_matrix[idx]
     )
-
     return(grm)
 }
